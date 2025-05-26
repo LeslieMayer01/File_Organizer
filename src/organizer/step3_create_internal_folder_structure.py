cd76f2e9d95452ac903c2eced11d247324aefb7e
Jorge David Duque A. <jorduque16@gmail.com>
2025-05-26 17:56:08 -0500

    feat: refactor step3

--- src/organizer/step3_create_internal_folder_structure.py
+++ src/organizer/step3_create_internal_folder_structure.py
@@ -1,32 +1,87 @@
 import os
+import shutil
 from datetime import datetime
-
+from typing import List, Dict, Any
 import pandas as pd
-
 import config
 
+# Variable global para controlar modo simulación
+MODO_SIMULACION = True
 
-def añadir_fecha_y_hora_al_nombre(archivo):
+
+def añadir_fecha_y_hora_al_nombre(archivo: str) -> str:
     fecha_hora_actual = datetime.now().strftime("%d-%m-%Y_%H-%M")
     nombre, extension = os.path.splitext(archivo)
     nuevo_nombre = f"{fecha_hora_actual}-{nombre}{extension}"
     return nuevo_nombre
 
 
-# Configuración
-archivo_reporte = "./reports/" + añadir_fecha_y_hora_al_nombre(
-    "Reporte_Gestion_Carpetas.xlsx"
-)
+def mover_carpeta(src: str, dst: str, reportes: list) -> None:
+    os.makedirs(dst, exist_ok=True)
+    destino_final = os.path.join(dst, os.path.basename(src))
+    if not MODO_SIMULACION:
+        shutil.move(src, destino_final)
+        estado = "MOVIDA"
+    else:
+        estado = "A MOVER"
+    reportes.append(
+        {
+            "Acción": "Mover Carpeta",
+            "Origen": src,
+            "Destino": destino_final,
+            "Estado": estado,
+        }
+    )
 
 
-def run():
-    print("✏️ Step 3: Renaming folders...")
-    create_folders(
-        config.FOLDER_TO_ORGANIZE, archivo_reporte, solo_reporte=False
-    )  # FALSE PARA CORRER TRUE PARA SIMULACIÓN
+def reorganizar_instancias_y_etapas(ruta_base: str, reportes: list) -> None:
+    instancia_paths = {
+        "C01": "01PrimeraInstancia",
+        "C02": "01PrimeraInstancia",
+        "C03": "01PrimeraInstancia",
+        "C04": "01PrimeraInstancia",
+        "C05": "01PrimeraInstancia",
+        "C06": "02SegundaInstancia",
+        "C08": "03RecursosExtraordinarios",
+        "C10": "04Ejecucion",
+    }
+
+    for carpeta in instancia_paths.values():
+        os.makedirs(os.path.join(ruta_base, carpeta), exist_ok=True)
+
+    ppal_dir = os.path.join(ruta_base, "01PrimeraInstancia", "C01Principal")
+    os.makedirs(ppal_dir, exist_ok=True)
+
+    for item in os.listdir(ruta_base):
+        path_item = os.path.join(ruta_base, item)
+        if os.path.isfile(path_item):
+            destino = os.path.join(ppal_dir, item)
+            if not MODO_SIMULACION:
+                shutil.move(path_item, destino)
+                estado = "MOVIDO"
+            else:
+                estado = "A MOVER"
+            reportes.append(
+                {
+                    "Acción": "Mover Archivo",
+                    "Origen": path_item,
+                    "Destino": destino,
+                    "Estado": estado,
+                }
+            )
+
+    for item in os.listdir(ruta_base):
+        path_item = os.path.join(ruta_base, item)
+        if os.path.isdir(path_item):
+            for prefijo, c_destino in instancia_paths.items():
+                if item.startswith(prefijo):
+                    mover_carpeta(
+                        path_item, os.path.join(ruta_base, c_destino), reportes
+                    )
+                    break
 
 
-def create_folders(ruta_base, archivo_excel, solo_reporte=True):
+def create_folders(ruta_base: str, file_excel: str, solo_reporte: bool = True):
     palabras_clave = {
         "C01Principal": [
             "principal",
@@ -36,6 +91,7 @@ def create_folders(ruta_base, archivo_excel, solo_reporte=True):
             "Ppal",
             "PRINCIPAL",
             "CuadernoUnico",
+            "01.Expediente Restitutucion 056314089001201800150  ST",
             "01 Unica Instancia",
         ],
         "C05MedidasCautelares": [
@@ -67,30 +123,42 @@ def create_folders(ruta_base, archivo_excel, solo_reporte=True):
             "indidente",
             "incidentes",
             "INCIDENTE",
-            "Incidente",
+            " Incidente",
             "Incidentes",
             "INCIDENTES",
         ],
+        "C06Apelacion": [
+            "Apelacion",
+            "apelacion",
+            "segunda instancia",
+            " Segunda Instancia",
+        ],
+        "C08Recurso": ["tutela", "Tutela", "Recurso", " Extraordinario"],
+        "C10EjecucionSentencia": [
+            "tramiteposterior",
+            "Traite Posterior",
+            "Ejecucion",
+            " ejecucion",
+        ],
     }
 
-    reportes = []
+    reportes: List[Dict[str, Any]] = []
 
     for root, dirs, _ in os.walk(ruta_base):
         for dir_name in dirs:
-            ruta_actual = os.path.join(root, dir_name)
+            if dir_name.startswith(
+                "053804"
+            ):  # CAMBIAR POR NUMERO DE IDENTIFICACION DEL JUZGADO
+                ruta_actual = os.path.join(root, dir_name)
+                reorganizar_instancias_y_etapas(ruta_actual, reportes)
 
-            if dir_name.startswith("053804"):
-                ruta_053804 = ruta_actual
-
-                # Crear 01PrimeraInstancia si no existen
-                # ni 01PrimeraInstancia ni 01UnicaInstancia
                 if not any(
-                    os.path.isdir(os.path.join(ruta_053804, nombre))
+                    os.path.isdir(os.path.join(ruta_actual, nombre))
                     for nombre in ["01PrimeraInstancia", "01UnicaInstancia"]
                 ):
-                    new = os.path.join(ruta_053804, "01PrimeraInstancia")
+                    new = os.path.join(ruta_actual, "01PrimeraInstancia")
                     estado = "A CREAR"
-                    if not solo_reporte:
+                    if not solo_reporte and not MODO_SIMULACION:
                         os.mkdir(new)
                         estado = "CREADA"
                     reportes.append(
@@ -101,16 +169,15 @@ def create_folders(ruta_base, archivo_excel, solo_reporte=True):
                         }
                     )
 
-                # Verificar si hay alguna carpeta que comience con C0
                 tiene_C0 = any(
                     d.startswith("C0")
-                    for d in os.listdir(ruta_053804)
-                    if os.path.isdir(os.path.join(ruta_053804, d))
+                    for d in os.listdir(ruta_actual)
+                    if os.path.isdir(os.path.join(ruta_actual, d))
                 )
                 if not tiene_C0:
-                    ruta_C0 = os.path.join(ruta_053804, "C01Principal")
+                    ruta_C0 = os.path.join(ruta_actual, "C01Principal")
                     estado = "A CREAR"
-                    if not solo_reporte:
+                    if not solo_reporte and not MODO_SIMULACION:
                         os.mkdir(ruta_C0)
                         estado = "CREADA"
                     reportes.append(
@@ -121,33 +188,25 @@ def create_folders(ruta_base, archivo_excel, solo_reporte=True):
                         }
                     )
 
-        # Segundo recorrido para renombrar según palabras clave
         for dir_name in dirs:
-            ruta_carpeta = os.path.join(root, dir_name)
-
-            # Omitir nombres ya correctos
+            ruta_carp = os.path.join(root, dir_name)
             if dir_name in palabras_clave.keys():
                 continue
-
             for nuevo_nombre, palabras in palabras_clave.items():
                 if any(palabra in dir_name for palabra in palabras):
                     ruta_nueva = os.path.join(root, nuevo_nombre)
-
                     if os.path.exists(ruta_nueva):
                         estado = "DUPLICADA"
                     else:
-                        if not solo_reporte:
+                        if not solo_reporte and not MODO_SIMULACION:
                             try:
-                                os.rename(ruta_carpeta, ruta_nueva)
+                                os.rename(ruta_carp, ruta_nueva)
                                 estado = "RENOMBRADA"
                             except Exception as e:
                                 estado = f"ERROR: {e}"
                         else:
                             estado = "A RENOMBRAR"
-                    if estado != "DUPLICADA":
-                        ruta = ruta_nueva
-                    else:
-                        ruta = ruta_carpeta
+                    ruta = ruta_carp if estado == "DUPLICADA" else ruta_nueva
                     reportes.append(
                         {
                             "Acción": "Renombrar Carpeta",
@@ -157,10 +216,18 @@ def create_folders(ruta_base, archivo_excel, solo_reporte=True):
                             "Ruta": ruta,
                         }
                     )
-                    # Evita múltiples renombramientos
-                    # para la misma carpeta
                     break
 
     df = pd.DataFrame(reportes)
-    df.to_excel(archivo_excel, index=False)
-    print(f"Reporte generado en: {archivo_excel}")
+    df.to_excel(file_excel, index=False)
+    print(f"Reporte generado en: {file_excel}")
+
+
+def run():
+    print("✏️ Step 3: Renaming folders...")
+    archivo_reporte = "./reports/" + añadir_fecha_y_hora_al_nombre(
+        "Reporte_Gestion_Carpetas.xlsx"
+    )
+    create_folders(
+        config.FOLDER_TO_ORGANIZE, archivo_reporte, solo_reporte=True
+    )  # Cambiar a False para ejecutar cambios reales"
